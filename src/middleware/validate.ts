493982030beb2fb8c2e4d64354c4febe167475bf
iuryliberato <iuryliberato@hotmail.com>
2024-12-03 15:03:13 -0300

    updated from ZodSchema to ZodType

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodSchema } from 'zod';
+import { ZodType } from 'zod';
 
-export const validate = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+export const validate = (schema: ZodType<unknown>) => (req: Request, res: Response, next: NextFunction) => {
     try {
         schema.parse(req.body);
         next();
